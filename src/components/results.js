20980fa1e7ce0882fbde82874bd34d8fa6e43847
mcast2185 <76968385+mcast2185@users.noreply.github.com>
2021-11-30 06:13:50 -0800

    update

---
+++ src/components/results.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Logo from "./logo";
+
+class Results extends Component {
+  render() {
+    return (
+      <div>
+        <h2> Results </h2>
+        <Logo/>
+      </div>
+    )
+  };
+};
+
+export default Results;
