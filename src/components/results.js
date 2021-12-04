9afca34ccff72c100ef05698820806382df7cdbe
mcast2185 <76968385+mcast2185@users.noreply.github.com>
2021-12-04 06:14:35 -0800

    update

--- src/components/results.js
+++ src/components/results.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+
 import Logo from "./logo";
+import SearchBar from './searchBar';
+
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 
 class Results extends Component {
+
+  handleSearchBarSubmit(query) {
+    this.props.fetchPostsWithQuery(query);
+  }
+
   render() {
     return (
       <div>
-        <h2> Results </h2>
-        <Logo/>
+        <Logo size={55}/>
+        <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
       </div>
     )
   };
 };
 
-export default Results;
+export default connect(null, actions)(Results);
