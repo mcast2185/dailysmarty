579a9eedf21f210cc25c99a1aae8778bce4885e3
mcast2185 <76968385+mcast2185@users.noreply.github.com>
2021-12-04 04:19:31 -0800

    update

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import thunk from "redux-thunk";
 
-import Results from "./components/results"
+import Results from "./components/results";
 import Home from "./components/home";
 import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 import "./style/main.scss";
 
 function main() {
@@ -18,7 +19,7 @@ function main() {
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={Home} />
+          <Route exact path="/" component={Home} />
           <Route path="/results" component={Results} />
         </Switch>
       </BrowserRouter>
--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
+import { connect} from "react-redux";
 
-class SearchBar extends Component {
+import * as actions from "../actions"
+
+class RecentPosts extends Component {
+
+  componentDidMount() {
+    this.props.fetchRecentPosts()
+  }
+
+
+  renderPosts = function() {
+    const posts = this.props.recentPosts.map((post, index) => {
+        if(index < 3) {
+            return (
+                <li key={index}>
+                    {post.title}
+                </li>
+            )   
+        }
+    })
+    return posts
+  }
+
 
   render(){
     return (
@@ -8,9 +30,7 @@ class SearchBar extends Component {
         <div className="recent-posts__wrapper">
             <div className="recent-posts__heading">Recent Posts</div>
             <ul className="recent-posts__posts">
-                <li>recent post 0</li>
-                <li>recent post 1</li>
-                <li>recent post 2</li>
+                {this.renderPosts()}
             </ul>
         </div>
       </div>
@@ -18,4 +38,10 @@ class SearchBar extends Component {
   };
 };
 
-export default SearchBar;
+function mapStateToProps(state) {
+  return {
+      recentPosts: state.posts.recentPosts
+  }
+}
+
+export default connect(mapStateToProps, actions)(RecentPosts);
